6f0ad1bd140947d2145d244caaa499d1856d9221
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-11-04 21:56:40 -0600

    finished with main tasks

--- queries.sql
+++ queries.sql
@@ -1,9 +1,38 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT productname, categoryid from product
+join category on product.categoryid = category.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select
+	O.Id,
+	S.CompanyName AS COMPANYNAMEEEE
+from
+	[Order] as O
+join Shipper as S
+	on S.Id = O.ShipVia
+where O.OrderDate < '2012-08-09'
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select
+	OD.OrderId, OD.ProductId, OD.Quantity, P.ProductName
+from
+[orderDetail] as OD
+join Product as P
+	on P.Id = OD.ProductId
+WHERE
+OD.OrderId = '10251'
+order BY
+P.ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select
+O.Id as "Order ID", Customer.CompanyName as "Customer Company Name", Employee.LastName as "Employee Last Name"
+from
+[Order] as O
+JOIN Customer
+on Customer.Id = O.CustomerId
+join Employee
+on Employee.Id = O.EmployeeId
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,55 @@
 // scheme-model
+const db = require('../data/db-config');
+
+module.exports = {
+    find() {
+        return db('schemes')
+    },
+    findById(id) {
+        const response = new Promise((resolve, reject)=>{
+            if (db('schemes').where({id}).first()) {
+                resolve(db('schemes').where({id}).first())
+            } else {
+                reject(null)
+            }
+        })
+        return response;
+    },
+    findSteps(id) {
+        return db('steps')
+        .join('schemes', 'steps.scheme_id', 'schemes.id')
+        .select('steps.step_number', 'steps.instructions', 'schemes.scheme_name')
+        .where({'schemes.id': id}).orderBy('step_number')
+    },
+    async add(scheme) {
+    const [id] = await db('schemes').insert(scheme);
+    return db('schemes').where({id}).first();
+    },
+    update(changes, id) {
+        const promise = new Promise (async (resolve, reject)=>{
+            const num = await db('schemes').where({id}).update(changes);
+            if (num) {
+                resolve(db('schemes').where({id}).first())
+            } else {
+                reject(null)
+            }
+        })
+        return promise;
+    },
+    remove(id) {
+        const promise = new Promise(async(resolve, reject)=>{
+            const user = await db('schemes').where({id}).first();
+            if (user) {
+                const num = await db('schemes').where({id}).del();
+                if (num) {
+                    resolve(user)
+                } else {
+                    reject(null)
+                }
+            } else {
+                reject(null)
+            }
+        })
+        return promise
+    }
+}
